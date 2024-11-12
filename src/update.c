d5ef3ffc4cf0cf2991ec3a75a0c6fb2ed26c8b62
Szymon Piętas <szymp230@gmail.com>
2024-11-12 13:10:52 +0100

    function which draw coordinate system has been added

--- src/update.c
+++ src/update.c
@@ -1,8 +1,38 @@
 #include "../include/update.h"
 
+#define TRANSLATION_X N_COL/2 
+#define TRANSLATION_Y N_ROW/2
+
 float convert(int ij, float scale, int translation)
 {
 	return (ij - translation)/scale;
+}
+
+void drawCoordinates(char T[N_ROW][N_COL])
+{
+	for(int i = 0; i < N_COL; i++)
+	{
+		for(int j = 0; j < N_ROW; j++)
+		{
+			if(convert(i, 20, TRANSLATION_X) == 0 && convert(j, 11, TRANSLATION_Y) == 0)
+			{
+				T[j][i] = '0';	
+			}
+			else if(convert(i, 20, TRANSLATION_X) == 0)
+			{
+				T[j][i] = '|';
+			}
+			else if(convert(j, 11, TRANSLATION_Y) == 0)
+			{
+				T[j][i] = '-';
+			}
+			else
+			{
+
+			}
+
+		}
+	}
 }
 
 void circle(char T[N_ROW][N_COL], float a, float R, float r, bool on)
@@ -34,11 +64,16 @@ void circle(char T[N_ROW][N_COL], float a, float R, float r, bool on)
 	}
 }
 
+void move(char object[N_ROW][N_COL], float dx, float dy)
+{
+		
+}
+
 void update(float up_ti, char T[N_ROW][N_COL])
 {
 
 	//loadFixedCharMatrixFromFile("data.txt", T);
-
+/*
 	float a = 0;
 	static float t = 0;
 	t = t + up_ti;
@@ -47,5 +82,7 @@ void update(float up_ti, char T[N_ROW][N_COL])
 	circle(T, a, 2, 1, 1);
 
 	circle(T, 1, 0.2, 0, 0);
+*/
+	drawCoordinates(T);
 
 }
